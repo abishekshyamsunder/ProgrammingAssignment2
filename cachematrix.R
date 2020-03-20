775405b59db026c199475e4f1ae38a1a4239b2e7
abishekshyamsunder <49049340+abishekshyamsunder@users.noreply.github.com>
2020-03-20 11:09:08 +0530

    Completed R Script
    This file contains the answer for the 2nd Programming assignment of the R Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below serve to store the inverse of the matrix in cache so as to reduce cost of computation
 
-## Write a short comment describing this function
-
+## The makeCacheMatrix function takes a matrix as argument and creates an object (which is a list), which contains the matrix itself along with its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  get <- function() x
+  setinv<-function(inverse) inv<<-inverse
+  getinv<-function() inv
+  list(mat = x,get = get ,setinv = setinv, getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+## The cacheSolve function takes this created object as argument, and returns the inverse if already exists, else calculates and returns
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if(is.null(inv))
+  {
+    print("Inverse not cached, Calculating...")
+    mat<-x$get()
+    inv<-solve(mat)
+    x$setinv(inv)
+    inv
+  }
+  else
+    return(inv)
 }
+## uncomment the below 3 lines of code to run an example with the identity matrix as input 
+#t<-matrix(c(1,0,0,0,1,0,0,0,1),nrow = 3,ncol=3)
+#tt<-makeCacheMatrix(t)
+#cacheSolve(tt)
+## Notice that the second time the cacheSolve(tt) function is called, the 'Calculating' line is not printed
+#cacheSolve(tt)
+
